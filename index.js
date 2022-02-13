0b044e94c8b2e0eb21a284ac2d72e4050a8f326f
Dayvid Santana <santana.dayvid@outlook.com>
2022-02-13 07:18:12 -0300

    cut script

---
+++ index.js
@@ -0,0 +1,5 @@
+let navigation = document.querySelector('.navigation');
+navigation.onclick = function(){
+     // adiciona e retira a classe 'active' sempre que navigation é clicado
+    navigation.classList.toggle('active')
+}
